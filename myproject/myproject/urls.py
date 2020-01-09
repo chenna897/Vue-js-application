f343cc4ef6f549a5b7449792cc2cfd13467ca7f9
chenna897 <kesava@asareri.com>
2020-01-09 18:50:53 +0100

    implementing jwt authentication

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -20,12 +20,15 @@ from django.views.generic import TemplateView
 from rest_framework_simplejwt import views as jwt_views
 from article import viewsets
 
+from rest_framework import routers
+from article.viewsets import ArticleViewSet
+router = routers.DefaultRouter()
+router.register(r'article', ArticleViewSet)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('article', TemplateView.as_view(template_name='index.html')),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('hello/', viewsets.HelloView.as_view(), name='hello'),
-    
 ]
